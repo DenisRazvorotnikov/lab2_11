e5520cdd3a23debe7f924497db1e0c794f6021d0
DenisRazvorotnikov <deniska_raz26@mail.ru>
2023-01-16 12:02:25 +0300

    Tasks

---
+++ idz.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+def print_msg(ms):
+
+    def printer(nm, fm):
+        data = ms.format(n=nm, f=fm)
+        return data
+    return printer
+
+
+if __name__ == '__main__':
+    d = "Dear {f} {n} you have done a great job"
+    another = print_msg(d)
+    print(another("Den", "Razvorotnikov"))
